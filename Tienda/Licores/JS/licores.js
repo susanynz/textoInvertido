d951d2105b9db861a4c4c363bbb94be6f22ef930
Malvaez26 <daniel.malvaezh@gmail.com>
2024-02-08 02:33:12 -0600

    Se agrego modo oscuro al modal. Así mismo se configuro archivo licores.js para que al agregar productos sumara al carrito de compras.

--- Tienda/Licores/JS/licores.js
+++ Tienda/Licores/JS/licores.js
@@ -22,16 +22,29 @@ const licoresMexicanos = [
       const productThumbnail = document.createElement('div');
       productThumbnail.classList.add('product-thumbnail');
   
+      // Crear la imagen
       const image = document.createElement('img');
       image.src = licor.img;
       image.alt = licor.name;
       image.addEventListener('click', () => mostrarDetallesProducto(licor));
   
+      // Crear el texto "Ver más"
+      const overlayText = document.createElement('div');
+      overlayText.classList.add('overlay-text');
+      overlayText.textContent = 'Ver más';
+  
+      // Agregar evento click al texto "Ver más"
+      overlayText.addEventListener('click', () => mostrarDetallesProducto(licor));
+  
+      // Agregar la imagen y el texto "Ver más" al thumbnail del producto
       productThumbnail.appendChild(image);
+      productThumbnail.appendChild(overlayText);
+  
       grid.appendChild(productThumbnail);
     });
   }
   
+
   // Función para mostrar los detalles del producto en el modal
   function mostrarDetallesProducto(producto) {
     const modalBody = document.getElementById('productModalBody');
@@ -50,11 +63,25 @@ const licoresMexicanos = [
   // Llama a la función para mostrar las miniaturas de los licores artesanales mexicanos
   mostrarMiniaturasLicoresMexicanos();
 
-// Función para agregar un producto al "huacal"
-    function echarAlHuacal(nombreProducto) {
+  // Función para agregar un producto a la lista de deseos
+  function echarAlHuacal(nombreProducto) {
     const producto = licoresMexicanos.find(licor => licor.name === nombreProducto);
-        console.log('Producto añadido al huacal:', producto);
-  }
+    console.log('Producto añadido al huacal:', producto);
+    // Incrementar el contador de productos en el huacal
+    const huacalNumber = document.getElementById('huacal-number');
+    huacalNumber.textContent = parseInt(huacalNumber.textContent) + 1;
+}
+
+// Obtener referencia al botón "Ver más"
+const verMasButton = document.getElementById('verMasButton');
+
+// Obtener referencia al modal
+const productModal = new bootstrap.Modal(document.getElementById('productModal'));
+
+// Agregar evento clic al botón "Ver más" para abrir el modal
+verMasButton.addEventListener('click', () => {
+    productModal.show(); // Mostrar el modal al hacer clic en el botón "Ver más"
+});
   
   // Función para agregar un producto a la lista de deseos
   function agregarAListaDeDeseos(nombreProducto) {
