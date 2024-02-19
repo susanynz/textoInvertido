f948f0f2a1e16d6c342a5e9a19123f7ac8d2c961
susanynz <susanynz>
2024-02-18 22:13:29 -0600

    ordenar carpetas

--- footer/scrip.js
+++
@@ -1,8 +0,0 @@
-
-// Volver a inicio con el logo del pie de página. 
-document.querySelector('.descripcion-section img').addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
